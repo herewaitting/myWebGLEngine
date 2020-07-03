02bf26cd4d420c00a74ecbce54285e10b6e345ef
shitingchun <stc2zyl1992@163.com>
2020-07-03 15:36:24 +0800

    起步

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: STC
+ * @Date: 2020-07-03 15:26:44
+ * @LastEditors: STC
+ * @LastEditTime: 2020-07-03 15:26:45
+ */ 
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: STC
+ * @Date: 2020-07-03 15:25:54
+ * @LastEditors: STC
+ * @LastEditTime: 2020-07-03 15:30:58
+ */ 
+const config = {
+    entry: './src/main.js',
+    output: {
+        filename: 'StcGLEngine.js',
+        path: './build'
+    }
+};
+  
+module.exports = config;
