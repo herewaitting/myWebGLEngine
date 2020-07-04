defd0b220fd4243b174c51fee18b3245537bee35
stc2zy <651978103@qq.com>
2020-07-04 14:20:41 +0800

    同步

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@
  * @Author: STC
  * @Date: 2020-07-03 15:26:44
  * @LastEditors: STC
- * @LastEditTime: 2020-07-03 15:26:45
+ * @LastEditTime: 2020-07-03 17:36:38
  */ 
+import Check from "./Core/Check.js";
+import * as glMat from "./Core/gl-matrix.js";
+export const StcGLEngine = {
+    Check : Check,
+    glMat : glMat
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,15 @@
  * @Author: STC
  * @Date: 2020-07-03 15:25:54
  * @LastEditors: STC
- * @LastEditTime: 2020-07-03 15:30:58
+ * @LastEditTime: 2020-07-03 17:29:47
  */ 
+const path = require('path');
 const config = {
     entry: './src/main.js',
     output: {
         filename: 'StcGLEngine.js',
-        path: './build'
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: "umd",
     }
 };
   
